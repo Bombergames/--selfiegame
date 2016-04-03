c4e1cda12264f1e1fda0cb9b3f261833b07ef166
Bombergames <johnwblum@gmail.com>
2016-04-03 13:00:53 -0400

    weights work better now
    they don't work, they just work better

--- Assets/GrabbableItemScript.cs
+++ Assets/GrabbableItemScript.cs
@@ -5,6 +5,7 @@ public class GrabbableItemScript : MonoBehaviour {
 
 	Quaternion originalRot;
 	bool beingGrabbed;
+	Transform grabber;
 	// Use this for initialization
 	void Start () {
 		originalRot = this.transform.rotation;
@@ -15,16 +16,18 @@ public class GrabbableItemScript : MonoBehaviour {
 	void Update () {
 		if(beingGrabbed)
 		{
-			GetComponentInChildren<Rigidbody>().Sleep();
+			this.transform.position = grabber.position + grabber.rotation * Vector3.forward * 1.5f;
 		}
 	}
 
 	public void Grab(Transform t)
 	{
+		grabber = t;
 		this.transform.SetParent(t);
 		//this.transform.localPosition = new Vector3(0, -0.2f, 0);
-		this.transform.position += t.rotation * Vector3.forward * 1f;
+		this.transform.position = t.position + t.rotation * Vector3.forward * 1.5f;
 		GetComponentInChildren<Rigidbody>().useGravity = false;
+		GetComponentInChildren<BoxCollider>().enabled = false;
 		beingGrabbed = true;
 		//this.transform.Translate(new Vector3(0, 0, 0));
 	}
@@ -35,6 +38,7 @@ public class GrabbableItemScript : MonoBehaviour {
 		this.transform.SetParent(null);
 		this.transform.rotation = originalRot;
 		GetComponentInChildren<Rigidbody>().useGravity = true;
+		GetComponentInChildren<BoxCollider>().enabled = true;
 		beingGrabbed = false;
 	}
 }
